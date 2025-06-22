7b24353ecb7f252fbffae5dd9949f5ee382c4aa1
zaougaeya <130233931+zaougaeya@users.noreply.github.com>
2025-06-22 18:13:23 +0100

    GitHub Actions workflow

--- karma.conf.js
+++ karma.conf.js
@@ -14,15 +14,12 @@ module.exports = function (config) {
     ],
     client: {
       jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
+        // configuration options for Jasmine
       },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true, // removes the duplicated traces
+      suppressAll: true,
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/angular-tailwind'),
@@ -34,8 +31,17 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true,
-  })
-}
+
+    // 🔧 CI-compatible browser config
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
+      }
+    },
+
+    singleRun: true,
+    restartOnFileChange: false,
+  });
+};
