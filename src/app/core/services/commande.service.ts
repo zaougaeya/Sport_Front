55ecb14bae06cea54106d7cf9d06f3a32c888187
zaougaeya <130233931+zaougaeya@users.noreply.github.com>
2025-06-22 15:11:25 +0100

    trigger: launch GitHub Actions pipeline

--- src/app/core/services/commande.service.ts
+++ src/app/core/services/commande.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class CommandeService {
   private readonly baseUrl = 'http://localhost:8087/api/commandes';
-  private readonly userId = '683cd30c75078b686d4e44b5'; // statique pour l'instant
+  private readonly userId = '683cd30c75078b686d4e44b5'; 
   private readonly token = 'eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiI2ODNjZDMwYzc1MDc4YjY4NmQ0ZTQ0YjUiLCJyb2xlIjoiVVNFUiIsImpvYiI6IkpPVUVVUiIsImlhdCI6MTc0ODgxNjY4NCwiZXhwIjoxNzQ4OTAzMDg0fQ.65eIQwo1CbuA1ovDUEV3F5t0Vf7ISKbqtQYffVGFFEjEoFg5IXjl0fBcZKxoLwWH';
 
   constructor(private http: HttpClient) {}
